83e205ec7de47179ee09e8b6dd69368ca800ba57
Stefan Riedmüller <s.riedmueller@phytec.de>
2026-09-24 16:30:46 +0200

    gstreamer-examples: vpu: Add scripts to playback H.26x encoded files
    Add small examples to playback H.264 and H.265 encoded files which have
    been saved before.
    
    Signed-off-by: Stefan Riedmüller <s.riedmueller@phytec.de>

---
+++ phytec-gstreamer-examples/isi/vpu/play_h265_mkv-file.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -n "$1" ]; then
+	FILENAME="$1"
+else
+	FILENAME=$(find . -name "*H.265_HD.mkv" -print -quit)
+fi
+
+if [ -z "$FILENAME" ]; then
+	echo "Error: No H.265 encoded file found."
+	exit 1
+fi
+
+if [ ! -r "$FILENAME" ]; then
+	echo "Error: File ${FILENAME} not accessable."
+	exit 1
+fi
+
+echo ""
+echo "Playing ${FILENAME} with gstreamer, break with Ctrl-C"
+echo "========================================================================="
+echo ""
+
+gst-launch-1.0 \
+filesrc location="$FILENAME" ! \
+matroskademux ! h265parse ! v4l2h265dec ! \
+queue ! waylandsink sync=true
